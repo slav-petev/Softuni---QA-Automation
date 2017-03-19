1b62d1479373cce633b3684000b90ce23d3c82da
Slav Petkov <Slav Petkov>
2017-03-20 00:46:30 +0200

    Solved Sign Of Integer Number problem

---
+++ C# Overview/CSharpOverview/05.SignOfIntegerNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.SignOfIntegerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(
+                Console.ReadLine());
+
+            string numberSign = GetNumberSign(number);
+
+            Console.WriteLine("The number {0} is {1}.",
+                number, numberSign);
+        }
+
+        private static string GetNumberSign(int number)
+        {
+            if (number < 0)
+                return "negative";
+            if (number == 0)
+                return "zero";
+
+            return "positive";
+        }
+    }
+}
