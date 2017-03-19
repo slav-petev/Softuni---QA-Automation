8ba21931fe7be3d252fd35ad91b545655caf0b55
Slav Petkov <Slav Petkov>
2017-03-20 00:41:24 +0200

    Solved Blank Receipt problem

---
+++ C# Overview/CSharpOverview/04.BlankReceipt/BlankReceiptExercise.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BlankReceipt
+{
+    class BlankReceiptExercise
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+
+        private static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+        }
+
+        private static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        private static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        private static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+    }
+}
