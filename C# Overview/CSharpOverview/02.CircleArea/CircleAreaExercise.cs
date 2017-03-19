e3e4c7b02fe1a29c0fbcccab2d63a5288d9e6b6f
Slav Petkov <Slav Petkov>
2017-03-19 23:36:00 +0200

    Solved problem Circle Area

---
+++ C# Overview/CSharpOverview/02.CircleArea/CircleAreaExercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CircleArea
+{
+    class CircleAreaExercise
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(
+                Console.ReadLine());
+
+            double circleArea = Math.PI *
+                radius * radius;
+
+            Console.WriteLine("{0:F12}", circleArea);
+        }
+    }
+}
