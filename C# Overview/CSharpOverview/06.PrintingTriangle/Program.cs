61c545c698057b255506537cc6ea9ff004a0165d
Slav Petkov <Slav Petkov>
2017-03-20 07:16:35 +0200

    Solved problem Printin Triangle

---
+++ C# Overview/CSharpOverview/06.PrintingTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumberToPrint = int.Parse(
+                Console.ReadLine());
+
+            for (int i = 1; i < maxNumberToPrint; ++i)
+            {
+                PrintLine(1, i);
+            }
+
+            PrintLine(1, maxNumberToPrint);
+
+            for (int i = maxNumberToPrint - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        private static void PrintLine(int startNumber, int endNumber)
+        {
+            for (int i = startNumber; i <= endNumber; ++i)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
