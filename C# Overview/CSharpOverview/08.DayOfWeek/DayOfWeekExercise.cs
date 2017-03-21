3ed5dbd07bcc177b1ed5536ed0e9a071315acfa0
Slav Petkov <Slav Petkov>
2017-03-21 19:45:38 +0200

    Solved problem DayOfWeek

---
+++ C# Overview/CSharpOverview/08.DayOfWeek/DayOfWeekExercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.DayOfWeek
+{
+    class DayOfWeekExercise
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays =
+            {
+                "Monday", "Tuesday",
+                "Wednesday", "Thursday",
+                "Friday", "Saturday",
+                "Sunday"
+            };
+
+            int day = int.Parse(
+                Console.ReadLine());
+
+            if (day <= 0 || day > weekDays.Length)
+                Console.WriteLine("Invalid Day!");
+            else
+                Console.WriteLine(weekDays[day - 1]);
+        }
+    }
+}
