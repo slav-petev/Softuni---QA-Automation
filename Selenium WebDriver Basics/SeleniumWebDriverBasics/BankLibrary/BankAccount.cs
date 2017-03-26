d80b8f8cb46e105a5eb17bb247dff5351981bceb
slav_ <slav_@DESKTOP-JQR7HUT>
2017-03-26 12:20:11 +0300

    Added Unit Tests for BankAccount class

---
+++ Selenium WebDriver Basics/SeleniumWebDriverBasics/BankLibrary/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BankLibrary
+{
+    public class BankAccount
+    {
+        decimal amount;
+
+        public BankAccount(decimal initAmount)
+        {
+            if (initAmount < 0)
+                throw new ArgumentException("Amount can not be negative!");
+
+            this.Amount = initAmount;
+        }
+
+        public decimal Amount { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Deposit can not be negative!");
+            }
+            this.Amount += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Cannot withdraw negative amount!");
+
+            if (this.Amount <= 0)
+                throw new InvalidOperationException("Your account is empty");
+
+            if (amount > this.Amount)
+                throw new InvalidOperationException("Cannot withdraw more money than you have!");
+
+            if (amount < 1000)
+            {
+                amount += (amount * 0.02m);
+                this.Amount -= amount;
+            }
+            else
+            {
+                amount += (amount * 0.05m);
+                this.Amount -= amount;
+            }
+        }
+    }
+}
---
+++ Selenium WebDriver Basics/SeleniumWebDriverBasics/BankLibraryTests/BankAccountTests.cs
@@ -0,0 +1,63 @@
+﻿using BankLibrary;
+using NUnit.Framework;
+using System;
+
+namespace BankLibraryTests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(30)]
+        [Category("BankLibraryTests")]
+        public void ShouldCreateBankAccountWithNonNegativeAmountCorrectly(decimal initialAmount)
+        {
+            var bankAccount = new BankAccount(initialAmount);
+
+            Assert.AreEqual(initialAmount, bankAccount.Amount);
+        }
+
+        [Test]
+        [Category("BankLibraryTests")]
+        public void ShouldThrowArgumentExceptionWhenCreatingBankAccountWithNegativeAmount()
+        {
+            Assert.Throws<ArgumentException>(() => new BankAccount(-1));
+        }
+
+        [Test]
+        [Category("BankLibraryTests")]
+        public void ShouldThrowArgumentExceptionWhenTryingToWithdrawNegativeAmount()
+        {
+            var bankAccount = new BankAccount(10);
+
+            Assert.Throws<ArgumentException>(() => bankAccount.Withdraw(-5));
+
+        }
+
+        [Test]
+        [Category("BankLibraryTests")]
+        public void ShouldThrowArgumentExceptionWhenTryingToWithdrawMoreMoneyThanYouHave()
+        {
+            var bankAccount = new BankAccount(10);
+
+            Assert.Throws<InvalidOperationException>(() => bankAccount.Withdraw(15));
+        }
+
+        [Test]
+        [Category("BankLibraryTests")]
+        [TestCase(300, 200, 96)]
+        [TestCase(800, 800, -16)]
+        [TestCase(2000, 1000, 950)]
+        [TestCase(2000, 2000, -100)]
+        public void ShouldWithdrawAmountCorrectly(decimal initialAmount,
+            decimal withdrawAmount, decimal expectedAmount)
+        {
+            var bankAccount = new BankAccount(initialAmount);
+
+            bankAccount.Withdraw(withdrawAmount);
+
+            Assert.AreEqual(expectedAmount, bankAccount.Amount);
+        }
+    }
+}
